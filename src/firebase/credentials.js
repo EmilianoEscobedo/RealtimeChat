bcef1b46eef34bc44e040a291221a6d8b52c6ed0
Emiliano Escobedo <83373185+EmilianoEscobedo@users.noreply.github.com>
2022-10-27 23:20:29 -0300

    Update credentials.js

--- src/firebase/credentials.js
+++ src/firebase/credentials.js
@@ -2,11 +2,6 @@
 import {initializeApp} from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB4i6vilM2fTupGjGOATPTghfZ3t_Yu3Y4",
-  authDomain: "talk-it-chat.firebaseapp.com",
-  projectId: "talk-it-chat",
-  storageBucket: "talk-it-chat.appspot.com",
-  messagingSenderId: "992840502289",
-  appId: "1:992840502289:web:bf7f35b46e8e525dd0f5b0",
+  // Private credentials
 };
 export const app = initializeApp(firebaseConfig);
